36706b40c89215aa99daab03cfc24b8afa56253d
myelin0 <emi.veisllari@gmail.com>
2022-01-26 00:17:51 +0100

    Fix Linters

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
-import "./style.css";
+// import _ from 'lodash';
+import './style.css';
 
 // function component() {
 //   const element = document.createElement("div");
@@ -15,33 +15,31 @@ import "./style.css";
 
 const toDoTasks = [
   {
-    description: "Make breakfast",
+    description: 'Make breakfast',
     completed: false,
     index: 0,
   },
   {
-    description: "Drink Juice",
+    description: 'Drink Juice',
     completed: true,
     index: 1,
   },
   {
-    description: "Go out",
+    description: 'Go out',
     completed: false,
     index: 2,
-  }
+  },
 ];
 
-const List= document.querySelector('.List');
+const List = document.querySelector('.List');
 
 const createList = () => {
-toDoTasks.forEach((task)=>{
-const element= document.createElement('li');
-element.innerHTML = `<div class="left"><i class="far fa-square"></i> ${task.description} </div><i class="fa fa-ellipsis-v"></i>`
-element.classList.add('rows')
-List.append(element);
-})
-}
+  toDoTasks.forEach((task) => {
+    const element = document.createElement('li');
+    element.innerHTML = `<div class="left"><i class="far fa-square"></i> ${task.description} </div><i class="fa fa-ellipsis-v"></i>`;
+    element.classList.add('rows');
+    List.append(element);
+  });
+};
 
-
-window.onload = createList() 
- 
+window.onload = createList();
--- todolist/webpack.config.js
+++ todolist/webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
-      template: "./src/index.html",
+      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
