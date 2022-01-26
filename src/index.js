969005b57e93e9249c181696b41f96247dcf07dc
myelin0 <emi.veisllari@gmail.com>
2022-01-26 01:21:41 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-// import _ from 'lodash';
+
 import './style.css';
 
-// function component() {
-//   const element = document.createElement("div");
-
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(" ");
-//   element.classList.add('hello');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
 
 const toDoTasks = [
   {
